3728c5e6578311917408bd2a3a5f13aeb2c059c3
poojashankarssssss <poojashankar9035@gmail.com>
2026-04-09 09:31:07 +0530

    first commit

---
+++ lab0/src/main/java/com/example/lab/App.java
@@ -0,0 +1,13 @@
+package com.example.lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
