ffb7bf54413a558a6b6e09657a3cb8da7d3ad74b
poojashankarssssss <poojashankar9035@gmail.com>
2026-04-16 15:40:59 +0530

    first commit

---
+++ pooja/jenkins1/src/main/java/mvn/jenkins1/App.java
@@ -0,0 +1,13 @@
+package mvn.jenkins1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
